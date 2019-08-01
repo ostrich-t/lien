0991238168f162fa58c20a53ae8277a6a2f2dfb3
cb50one <txmalon0628@gmail.com>
2019-08-01 16:28:35 +0900

    update puma deamon true

--- config/puma.rb
+++ config/puma.rb
@@ -3,4 +3,5 @@ app_root = File.expand_path("../..", __FILE__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
 pidfile "#{app_root}/tmp/pid"
 state_path "#{app_root}/tmp/state"
+daemonize true
 activate_control_app
